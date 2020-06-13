95f561d70a44fb7c343be38678ee9b59db1cbe41
Fernando Almeida <fernando.bks@hotmail.com>
2020-06-13 19:01:14 -0300

    fix fetch data react

--- front/src/pages/Test.jsx
+++ front/src/pages/Test.jsx
@@ -3,15 +3,18 @@ import axios from 'axios';
 import styles from '../styles/Test.module.sass';
 
 const Test = () => {
-  const [data, setData] = useState({});
-  useEffect(async() => {
-    const result = await axios('http://127.0.0.1:8000/api/teste');
-    setData(result.data);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData(){
+      const result = await axios('http://127.0.0.1:8000/api/teste');
+      setData(result.data);
+    }
+    fetchData();
   }, [])
   return (
     <div className={styles.page}>
       <h1>Just a test</h1>
-  <p> DataContent: {data.teste}</p>
+        <p> DataContent: {data}</p>
     </div>
   );
 };
