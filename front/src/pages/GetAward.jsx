6c510813e0df4973f9fe06eae5f605a660ed75c9
Thamyres Guedes <thataguedis@gmail.com>
2020-06-14 02:22:39 -0300

    🔥 Removing unused files

--- front/src/pages/GetAward.jsx
+++ front/src/pages/GetAward.jsx
@@ -3,8 +3,6 @@ import '../styles/reset.css';
 import '../styles/base.sass';
 import styles from '../styles/GetAward.module.sass';
 import Navbar from '../components/Navbar';
-import iconWhatsApp from '../assets/whatsapp.svg';
-import iconFacebook from '../assets/facebook.svg';
 
 const Awards = () => {
   return (
@@ -34,8 +32,6 @@ const Awards = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-
-
           </section>
       </main>
     </div>
