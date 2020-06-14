a629ed2fd0fddee06914664322c8660189372196
Thamyres Guedes <thataguedis@gmail.com>
2020-06-14 10:32:01 -0300

    🚧 Preparing to integrate.. But I have a little CORS problem...

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Layout from '../layouts/index';
 import styles from '../styles/Main.module.sass';
 
 const Main = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+        "Content-Type": "application/json;charset=UTF-8"
+      }
+    };
+    async function fetchData(){
+      try {
+        const { data } = await axios.get('http://127.0.0.1:8000/activities/board', config);
+        setData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(Object.keys(error), error.message)
+      }
+    }
+    fetchData();
+  }, [])
   return (
     <Layout showPoints={true}>
       <section className={styles.activities}>
