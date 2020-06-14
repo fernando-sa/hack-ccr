bec4321a96679ef2aeb89940af2b7b6f991c5819
Thamyres Guedes <thataguedis@gmail.com>
2020-06-14 17:19:40 -0300

    ✨ Award integrated

--- front/src/pages/awards/index.jsx
+++ front/src/pages/awards/index.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { URI_API, URI_CONFIG } from '../../variables';
 import Layout from '../../layouts/index';
 import TargeShare from '../../components/TargeShare';
 import styles from '../../styles/Awards.module.sass';
 
 const Awards = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        const { data } = await axios.get(URI_API + '/prizes/board', URI_CONFIG);
+        setData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(Object.keys(error), error.message)
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <Layout showPoints={true} pageTitle="Prêmios">
       <TargeShare />
       <section className={styles.activities}>
         <h1 className={styles.title}>Prêmios disponíveis</h1>
-        {[1,2,3,4,5,6].map((item) => (
+        {data.map((item) => (
           <a href={`/premios/${item}`} className={styles.card}>
-            <h2 className={styles.cardTitle}>Manutenção com 50% off</h2>
+            <h2 className={styles.cardTitle}>{item.name}</h2>
             <div className={styles.cardContent}>
-              <p>Agende uma troca de óleo grátis na oficina do Marcão!</p>
+              <p>{item.description}</p>
             </div>
             <footer className={styles.cardFooter}>
-              Responder à pesquisa
+              Use <strong>{item.price} pontos</strong> para resgatar
             </footer>
           </a>
         ))}
---
+++ front/src/variables.js
@@ -0,0 +1,9 @@
+export const URI_API ='http://127.0.0.1:8000/api';
+export const URI_CONFIG = {
+  headers: {
+    "Access-Control-Allow-Origin": URI_API,
+    "Access-Control-Allow-Headers": "Authorization", 
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+    "Content-Type": "application/json;charset=UTF-8"
+  }
+};
