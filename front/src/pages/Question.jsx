0a7cc685f2056067361528092efe5bd9368510a0
Thamyres Guedes <thataguedis@gmail.com>
2020-06-14 02:22:52 -0300

    ✨ Page Question

---
+++ front/src/pages/Question.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../styles/reset.css';
+import '../styles/base.sass';
+import styles from '../styles/Question.module.sass';
+import Navbar from '../components/Navbar';
+
+const Question = () => {
+  return (
+    <div className="layout">
+      <header className={styles.navbar}>
+        <Navbar />
+        <span className={styles.navTitle}>Pergunta</span>
+      </header>
+      <main className={styles.mainContent}>
+          <header className={styles.track}>
+            1 de 5 perguntas
+          </header>
+          <div className={styles.questionBlock}>
+            <h1 className={styles.title}>Intraestrutura</h1>
+            <p className={styles.question}>O que você achou dessa parada?</p>
+            <ul className={styles.options}>
+              {([1,2,3,4]).map((item) => (
+                <li>
+                  <label htmlFor={`op0${item}`} className={styles.label}>
+                    <input type="radio" name="option" id={`op0${item}`}/>
+                    Infraestrutura muito boa
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <footer className={styles.footer}>
+            <button type="button" className={styles.btnSendAnswer}>Responder</button>
+          </footer>
+      </main>
+    </div>
+  );
+};
+
+export default Question;
--- front/src/routes.js
+++ front/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Awards from './pages/Awards';
 import GetAward from './pages/GetAward';
+import Question from './pages/Question';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Routes = () => (
       <Route path="/" exact component={Main} />
       <Route path="/premios" exact component={Awards} />
       <Route path="/resgatar" exact component={GetAward} />
+      <Route path="/pergunta" exact component={Question} />
     </Switch>
   </BrowserRouter>
 );
