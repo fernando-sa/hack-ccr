ae1bcb677c5fa9aa66b3d812d39e6e30619cef06
Thamyres Guedes <thataguedis@gmail.com>
2020-06-14 22:33:19 -0300

    ✨ Page 404

--- front/src/pages/Page404.jsx
+++ front/src/pages/Page404.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Layout from '../layouts/index';
+import styles from '../styles/Page404.module.sass';
+import iconError from '../assets/error-404.svg';
 
 const Page404 = () => {
   return (
-    <div>
-      <h1>Página não Encontrada</h1>
-    </div>
+    <Layout>
+      <div className={styles.notFound}>
+        <h1 className={styles.title}>Página não encontrada</h1>
+        <img src={iconError} alt="Erro 404" className={styles.imageError}/>
+        <p>Não encontramos a página que você solicitou</p>
+        <a href="/" className={styles.goToHome}>Vá para a página inicial</a>
+      </div>
+    </Layout>
   );
 };
 
