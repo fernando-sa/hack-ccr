4d7d826be85dc0100ec05438a4ec3cd3fb979c46
Fernando Almeida <fernando.bks@hotmail.com>
2020-06-14 21:33:25 -0300

    added wpp share link

--- front/src/components/TargePoints.jsx
+++ front/src/components/TargePoints.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { URI_API, URI_CONFIG } from '../variables';
+
 import styles from '../styles/TargePoints.module.sass';
 
 const TargePoints = () => {
+  const [points, setData] = useState(0);
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        const { data } = await axios.get(URI_API + '/user/points/1', URI_CONFIG);
+        setData(data);
+      } catch (error) {
+        console.log(Object.keys(error), error.message)
+      }
+    }
+    fetchData();
+  }, []);  
   return (
     <section className={styles.targePoints}>
       Você possui:
-      <strong>2.000</strong>
+      <strong>{points}</strong>
       pts
     </section>
   );
